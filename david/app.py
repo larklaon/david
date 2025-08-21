49fe905963532f2e9458b558b63549b0599c1323
song yeong jun <larklaon@naver.com>
2025-08-21 18:19:01 +0900

    Update app.py

--- david/app.py
+++ david/app.py
@@ -124,6 +124,10 @@ def home():
 def menu():
     return render_template('menu.html')
 
+@app.route("/test2")
+def test2():
+    return render_template('test2.html')
+
 # 메인 실행 부분
 if __name__ == '__main__':
     # debug=True 설정
